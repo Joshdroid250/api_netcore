97e68b0bcb622465a7b6774122db3bdd695be91b
Joshua Campos Gutiérrez <gutierrez25g@gmail.com>
2024-10-24 12:14:13 -0600

    adding endpoints to add, getbyid and getall

--- Api_redes/Controllers/UsuarioController.cs
+++ Api_redes/Controllers/UsuarioController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 using Api_redes.Models;
+using Microsoft.EntityFrameworkCore;
 
 
 
@@ -19,22 +20,73 @@ namespace Api_redes.Controllers
         }
 
         [HttpGet]
-        [Route("Lista")]
+        [Route("")]
 
         public IActionResult Lista() { 
             List<User> users = new List<User>();
 
             try
             {
-                users = _tuDbContext.Users.ToList();
+                 users = _tuDbContext.Users.ToList();
+
+                if (users == null || !users.Any())
+                {
+                    return StatusCode(StatusCodes.Status204NoContent, new { mensaje = "No hay usuarios disponibles", response = users });
+                }
 
                 return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = users });
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
+                // Registro del error si es necesario (log)
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = "Ocurrió un error", detalle = ex.Message });
+            }
+        }
 
-                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = users });
+        [HttpGet]
+        [Route("{id:int}")]
+
+        public IActionResult GetUsersByID(int id)
+        {
+            User oUsers = _tuDbContext.Users.Find(id);
+
+            if (oUsers == null)
+            {
+                return BadRequest("User not found");
+            }
+
+            try
+            {
+               return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = oUsers });
+            }
+            catch (Exception ex)
+            {
+                // Registro del error si es necesario (log)
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = "Ocurrió un error", detalle = ex.Message });
+            }
+        }
+
+        [HttpPost]
+
+        [Route("")]
+
+        public IActionResult AddUser([FromBody] User oUser) {
+
+                       
+            try
+            {
+                _tuDbContext.Users.Add(oUser);
+                _tuDbContext.SaveChanges();
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok"});
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok"});
 
             }
         }
+
     }
 }
